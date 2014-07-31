915d77880555fab972d217700d61586c61117fae
youthpasses <543496951@qq.com>
2014-07-31 09:44:39 +0800

    Initial Commit

---
+++ MKInputBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MKInputBar
+//
+//  Created by makai on 14-7-31.
+//  Copyright (c) 2014年 makai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MKInputBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MKInputBar
+//
+//  Created by makai on 14-7-31.
+//  Copyright (c) 2014年 makai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
